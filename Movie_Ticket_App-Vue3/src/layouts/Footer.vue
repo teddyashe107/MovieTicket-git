27991d2523c07e1bd7a8d7963e81fc22487d4f9d
teddyashe17 <teddyashe107@gmail.com>
2021-11-27 00:51:20 +0300

    adding intractive footer

--- Movie_Ticket_App-Vue3/src/layouts/Footer.vue
+++ Movie_Ticket_App-Vue3/src/layouts/Footer.vue
@@ -4,12 +4,29 @@
 
 <template>
   <footer class="mt-10">
-    <BaseCard class="items-center">
-      <div class="flex justify-between items-center">
-        <div>
-          <p class="text-2xl">All rights reserved © Teddy</p>
+    <div class="container-fluid mx-auto">
+      <BaseCard class="">
+        <div class="flex justify-between items-center">
+          <a
+            target="_blank"
+            href=""
+            style="position: fixed; bottom: 14px; right: 80px"
+            class="
+              py-3
+              px-5
+              bg-green-500
+              text-white
+              rounded
+              font-normal
+              leading-4
+            "
+            >Get Notified</a
+          >
+          <div>
+            <p>All rights reserved © teddy 2021</p>
+          </div>
         </div>
-      </div>
-    </BaseCard>
+      </BaseCard>
+    </div>
   </footer>
 </template>
