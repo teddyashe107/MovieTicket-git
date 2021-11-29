1df13f6675ff976e2604f5b61210124afd4e2d3f
teddyashe17 <teddyashe107@gmail.com>
2021-11-29 20:52:05 +0300

    adding admin dashboard and charts

--- Movie_Ticket_App-Vue3/vite.config.js
+++ Movie_Ticket_App-Vue3/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), ViteComponents()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+		},
+	},
 
 	optimizeDeps: {
 		include: ['zen-observable', '@vue/apollo-composable'],
