b80406f088309c0e84d2e23c8a7270ddd59b0d63
teddyashe17 <teddyashe107@gmail.com>
2021-11-30 21:52:03 +0300

    updating my card view

--- Movie_Ticket_App-Vue3/src/views/Home.vue
+++ Movie_Ticket_App-Vue3/src/views/Home.vue
@@ -1,10 +1,10 @@
 <script setup>
 import {useQuery, useResult, useApolloClient } from '@vue/apollo-composable'
 import {gql} from 'graphql-tag'
-
+/*
 const {resolveClient} = useApolloClient()
 const client = resolveClient('')
-
+*/
 
 
 const getScheduledMovie = gql `
@@ -22,14 +22,14 @@ query getScheduledMovie {
 
 `
 
-
+/*
 const {result} = useQuery(getScheduledMovie);
 const scheduledMovies = useResult(result, null, (data) => data.schedule_movies)
 
 const myMovies = () => {
   console.log(client)
 }
-
+*/
 const movies = [
   {
     id: 1,
@@ -80,57 +80,126 @@ const show = () => {
 </script>
 
 <template>
+  <div class="text-xl font-semibold">Recommended Movies</div>
   <div>
-    <div>
-      <h1 class="text-3xl font-mono text-left">Recommended Movies</h1>
-      <button @click="myMovies">my Movies</button>
-      {{ scheduledMovies }}
-    </div>
-
+    <!-- card example -->
     <div
       class="
         grid
-        xs:grid-cols-1
         sm:grid-cols-2
+        xs:grid-cols-1
         md:grid-cols-3
         xl:grid-cols-4
         gap-4
       "
     >
-      <div v-for="movie in movies" :key="movie.id">
-        <BaseCard class="text-center hover:bg-gray-100 hover:text-gray-900">
-          <img
-            class="w-70 h-60 m-auto shadow-sm avatar-md rounded"
-            :src="movie.image_path"
-          />
-          <p class="text-base mt-4 text-left">
-            rating
-            <span class="font-bold text-red-700">{{ movie.rating }}</span>
-          </p>
-          <p class="text-left text-xl font-bold">{{ movie.name }}</p>
-          <p class="my-2 text-lg text-gray-500 mb-3 text-left">
-            {{ movie.type }}
-          </p>
+      <div class="container mx-auto" v-for="movie in movies" :key="movie.id">
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full p-4">
+            <a
+              href=""
+              class="
+                c-card
+                block
+                bg-white
+                shadow-lg
+                hover:shadow-xl
+                rounded-lg
+                overflow-hidden
+              "
+            >
+              <div class="relative pb-96 overflow-hidden">
+                <img
+                  class="absolute inset-0 h-full w-full object-cover"
+                  src="/images/teda.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="p-4">
+                <span
+                  class="
+                    inline-block
+                    px-2
+                    py-1
+                    leading-none
+                    bg-pink-200
+                    text-pink-800
+                    rounded-full
+                    font-semibold
+                    uppercase
+                    tracking-wide
+                    text-xs
+                  "
+                  >November 11/30/21</span
+                >
+                <h2 class="mt-2 mb-2 font-bold">
+                  Purus Ullamcorper Inceptos Nibh
+                </h2>
 
-          <RouterLink
-            block
-            :to="{ name: 'MovieDetails', params: { id: movie.id } }"
-            class="
-              bg-pink-600
-              hover:bg-gray-900 hover:text-white
-              text-white
-              mb-2
-              rounded-full
-            "
-          >
-            show details
-          </RouterLink>
-        </BaseCard>
+                <div class="mt-3 flex items-center">
+                  <span class="text-sm font-semibold">ab</span>&nbsp;<span
+                    class="font-bold text-xl"
+                    >45,00</span
+                  >&nbsp;<span class="text-sm font-semibold">€</span>
+                </div>
+              </div>
+
+              <div class="p-4 mt-0 flex items-center text-sm">
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 fill-current text-yellow-500"
+                >
+                  <path
+                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+                  ></path></svg
+                ><svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 fill-current text-yellow-500"
+                >
+                  <path
+                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+                  ></path></svg
+                ><svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 fill-current text-yellow-500"
+                >
+                  <path
+                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+                  ></path></svg
+                ><svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 fill-current text-yellow-500"
+                >
+                  <path
+                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+                  ></path></svg
+                ><svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 fill-current text-gray-400"
+                >
+                  <path
+                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+                  ></path></svg
+                ><span class="ml-2">Rate this movie</span>
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<style lang="sass">
+.c-card
+img
+  transition: transform .3s ease-in-out
 
-
-<style>
+:hover
+  img
+    transform: scale(1.05)
 </style>
