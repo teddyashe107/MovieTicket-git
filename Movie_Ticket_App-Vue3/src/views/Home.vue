2cb46360baddb1d62c11fbbc87a93a2337da24b1
teddyashe17 <teddyashe107@gmail.com>
2021-12-11 00:23:34 +0300

    working with the admin side

--- Movie_Ticket_App-Vue3/src/views/Home.vue
+++ Movie_Ticket_App-Vue3/src/views/Home.vue
@@ -1,14 +1,24 @@
 <script setup>
-import {useQuery, useResult, useApolloClient } from '@vue/apollo-composable'
+import {useQuery, useResult, useApolloClient, useSubscription } from '@vue/apollo-composable'
 import {gql} from 'graphql-tag'
+import {useRoute,useRouter} from 'vue-router';
+import{computed, reactive, ref} from'vue'
+import { StarIcon } from '@heroicons/vue/solid'
+
+  const product = {
+    rating: 3.9
+  }
 /*
 const {resolveClient} = useApolloClient()
 const client = resolveClient('')
 */
+const router = useRouter()
+const route = useRoute()
+
 
 
 const getScheduledMovie = gql `
-query getScheduledMovie {
+query getMovies {
   schedule_movies {
     id
     movie {
@@ -20,21 +30,25 @@ query getScheduledMovie {
   }
 }
 
+
 `
 
-/*
+
 const {result} = useQuery(getScheduledMovie);
 const scheduledMovies = useResult(result, null, (data) => data.schedule_movies)
 
+//console.log(scheduledMovies)
+/*
 const myMovies = () => {
   console.log(client)
 }
 */
-const movies = [
+const movies = ref([
   {
     id: 1,
     rating: 5,
     name: "Lost",
+    description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "horror",
     image_path: "/images/teda.jpg",
   },
@@ -42,6 +56,7 @@ const movies = [
     id: 2,
     rating: 4.5,
     name: "Prison Break",
+     description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "action",
     image_path: "/images/teda.jpg",
   },
@@ -49,6 +64,7 @@ const movies = [
     id: 3,
     rating: 4,
     name: "The Vampire",
+     description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "horror",
     image_path: "/images/teda.jpg",
   },
@@ -56,6 +72,7 @@ const movies = [
     id: 4,
     rating: 3,
     name: "Love",
+     description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "romantic",
     image_path: "/images/teda.jpg",
   },
@@ -63,6 +80,7 @@ const movies = [
     id: 5,
     rating: 2,
     name: "kid",
+     description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "family",
     image_path: "/images/teda.jpg",
   },
@@ -70,124 +88,167 @@ const movies = [
     id: 6,
     rating: 1,
     name: "noon",
+     description: 'ServicesBrandingDesignMarketingAdvertisementCompanyAbout usContactJobsPress kitLegalTerms of usePrivacy policyCookie policy',
     type: "horror",
     image_path: "/images/teda.jpg",
   },
-];
+])
+
+
+function newPage (name) {
+ const movieDetail = computed(() => {
+  return movies.value.filter(data => data.name.includes(name))
+
+})
+ const dataP = JSON.parse(JSON.stringify(movieDetail.value))
+ // console.log(dataP[0].name)
+   router.push({
+     name: 'MovieDetails', 
+     params : { name: name},
+     query: {
+       name: dataP[0].name,
+       rating: dataP[0].rating,
+       description: dataP[0].description,
+       thumbnail: dataP[0].image_path
+     
+     }})
+}
+
+
+
 const show = () => {
   console.table(movies);
 };
 </script>
 
 <template>
-  <div class="text-xl font-semibold">Recommended Movies</div>
-  <div>
+  <ul>
+    <li v-for="movie in scheduledMovies" :key="movie.id">
+      <div class="flex flex-col">
+        <div class="flex-1">
+          <div class="text-xl font-semibold">{{ movie.movie.name }}</div>
+          <div class="text-sm font-semibold">{{ movie.movie.movie_type }}</div>
+        </div>
+        <div class="flex-1">
+          <div class="text-xl font-semibold">{{ movie.movie.price }}</div>
+          <div class="text-sm font-semibold">
+            {{ movie.movie.published_year }}
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+
+  <Hero />
+  <div
+    class="
+      flex flex-col
+      ml-auto
+      justify-center
+      max-w-full
+      bg-gray-900
+      h-20
+      tracking-tight
+      border-b
+      shadow-lg
+    "
+  >
+    <h1
+      class="
+        mx-auto
+        bg-gradient-to-r
+        from-purple-900
+        to-pink-300
+        bg-clip-text
+        text-transparent text-xs
+        font font-bold
+        sm:text-3xl
+      "
+    >
+      Recommended Movies
+    </h1>
+  </div>
+
+  <div class="pt-11">
     <!-- card example -->
     <div
       class="
         grid
-        sm:grid-cols-2
-        xs:grid-cols-1
-        md:grid-cols-3
+        sm:grid-cols-1 sm:container sm:mx-auto
+        xs:grid-cols-1 xs:container xs:mx-auto
+        md:grid-cols-2 md:container md:mx-auto
         xl:grid-cols-4
         gap-4
       "
     >
-      <div class="container mx-auto" v-for="movie in movies" :key="movie.id">
+      <div
+        class="container mx-auto"
+        v-for="movie in movies"
+        :key="movie.id"
+        @click="newPage(movie.name)"
+      >
         <div class="flex flex-wrap -mx-4">
-          <div class="w-full p-4">
-            <a
-              href=""
-              class="
-                c-card
-                block
-                bg-white
-                shadow-lg
-                hover:shadow-xl
-                rounded-lg
-                overflow-hidden
-              "
-            >
-              <div class="relative pb-96 overflow-hidden">
-                <img
-                  class="absolute inset-0 h-full w-full object-cover"
-                  src="/images/teda.jpg"
-                  alt=""
-                />
-              </div>
-              <div class="p-4">
-                <span
-                  class="
-                    inline-block
-                    px-2
-                    py-1
-                    leading-none
-                    bg-pink-200
-                    text-pink-800
-                    rounded-full
-                    font-semibold
-                    uppercase
-                    tracking-wide
-                    text-xs
-                  "
-                  >November 11/30/21</span
-                >
-                <h2 class="mt-2 mb-2 font-bold">
-                  Purus Ullamcorper Inceptos Nibh
-                </h2>
+          <div
+            class="
+              w-full
+              p-4
+              c-card
+              block
+              shadow-lg
+              hover:shadow-2xl
+              rounded-lg
+              overflow-hidden
+            "
+          >
+            <div class="relative pb-96 overflow-hidden">
+              <img
+                class="absolute inset-0 rounded-lg h-full w-full object-cover"
+                src="/images/The-Avengers-Poster-013.jpg"
+                alt=""
+                loading="lazy"
+              />
+            </div>
+            <div class="p-4">
+              <span
+                class="
+                  inline-block
+                  px-2
+                  py-1
+                  leading-none
+                  bg-pink-200
+                  text-pink-800
+                  rounded-full
+                  font-semibold
+                  uppercase
+                  tracking-wide
+                  text-xs
+                "
+                >November 11/30/21</span
+              >
 
-                <div class="mt-3 flex items-center">
-                  <span class="text-sm font-semibold">ab</span>&nbsp;<span
-                    class="font-bold text-xl"
-                    >45,00</span
-                  >&nbsp;<span class="text-sm font-semibold">€</span>
-                </div>
-              </div>
+              <h2 class="mt-2 mb-2 font-bold">
+                Purus Ullamcorper Inceptos Nibh
+              </h2>
 
-              <div class="p-4 mt-0 flex items-center text-sm">
-                <svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 fill-current text-yellow-500"
-                >
-                  <path
-                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                  ></path></svg
-                ><svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 fill-current text-yellow-500"
-                >
-                  <path
-                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                  ></path></svg
-                ><svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 fill-current text-yellow-500"
-                >
-                  <path
-                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                  ></path></svg
-                ><svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 fill-current text-yellow-500"
-                >
-                  <path
-                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                  ></path></svg
-                ><svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4 fill-current text-gray-400"
-                >
-                  <path
-                    d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                  ></path></svg
-                ><span class="ml-2">Rate this movie</span>
+              <div class="mt-3 flex items-center">
+                <span class="text-sm font-semibold">ab</span>&nbsp;<span
+                  class="font-bold text-xl"
+                  >45,00</span
+                >&nbsp;<span class="text-sm font-semibold">€</span>
               </div>
-            </a>
+            </div>
+
+            <div class="flex items-center">
+              <StarIcon
+                v-for="rating in [0, 1, 2, 3, 4]"
+                :key="rating"
+                :class="[
+                  movie.rating > rating ? 'text-pink-700' : 'text-gray-200',
+                  'h-5 w-5 flex-shrink-0',
+                ]"
+                aria-hidden="true"
+              />
+            </div>
           </div>
         </div>
       </div>
---
+++ Movie_Ticket_App-Vue3/src/views/SearchExample.vue
@@ -0,0 +1,111 @@
+<script setup>
+  import {ref, reactive, computed} from 'vue'
+  const search = ref('')
+  const open = ref(false)
+  import { StarIcon } from '@heroicons/vue/solid'
+
+  const product = {
+    rating: 2
+  }
+
+  const test = [0,1,2,3]
+  console.log(product.rating > test)
+
+  const list = ref([
+     {
+       id: 1,
+       name: 'John Doe',
+       age: 30,
+
+     },
+     {
+
+        id: 2,
+        name: 'Teddy Doe',
+        age: 40,
+      },
+      {
+        id: 3,
+        name: 'Elias Doe',
+        age: 50,
+      },
+     
+  ])
+
+// function onInputChange(e) {
+//  return list.filter((item) =>{
+//         
+//           return  item 
+//         }) 
+// }
+
+
+
+const serchedValue = computed(() => {
+  return list.value.filter(data =>
+  { return data.name.toLowerCase().includes(search.value.toLowerCase())})
+})
+
+
+
+
+// 
+//  function filteredList() {
+//         return list.filter((item) =>{
+//         
+//               return  item 
+//         })
+//     }
+
+
+</script>
+<template>
+  <div class="container mx-auto flex justify-center items-center flex-col">
+    <h1>Test Search</h1>
+    <div
+      class="
+        mt-8
+        container
+        flex flex-col
+        justify-center
+        items-center
+        bg-gray-900
+      "
+    >
+      <label for="search">Search Items</label>
+      <input
+        type="text"
+        class="
+          rounded-full
+          mb-3
+          bg-gray-800
+          outline-none
+          text-white
+          focus:shadow-none
+        "
+        placeholder="type here...."
+        @change="onInputChange"
+        v-model="search"
+      />
+    </div>
+
+    <ul>
+      <li v-for="item in serchedValue" :key="item">
+        <h1>{{ item.name }}</h1>
+        <p>{{ item.age }}</p>
+      </li>
+    </ul>
+
+    <div class="flex items-center">
+      <StarIcon
+        v-for="rating in [0, 1, 2, 3, 4]"
+        :key="rating"
+        :class="[
+          product.rating > rating ? 'text-pink-700' : 'text-gray-200',
+          'h-5 w-5 flex-shrink-0',
+        ]"
+        aria-hidden="true"
+      />
+    </div>
+  </div>
+</template>
