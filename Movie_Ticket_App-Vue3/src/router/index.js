ca2be75ef8f177e922fda4a1fb791db8567ce68a
teddyashe17 <teddyashe107@gmail.com>
2021-12-11 00:24:38 +0300

    vue router

--- Movie_Ticket_App-Vue3/src/router/index.js
+++ Movie_Ticket_App-Vue3/src/router/index.js
@@ -3,6 +3,15 @@ import Home from '../views/Home.vue';
 import NotFound from '../views/NotFound.vue';
 import Test from '../views/Test.vue';
 import MovieDetails from '../views/MovieDetails.vue';
+import Movies from '../views/admin/Movies.vue';
+import Profile from '../views/admin/Profile.vue';
+import Reservatons from '../views/admin/Reservations.vue';
+import ScheduledMovies from '../views/admin/ScheduledMovies.vue';
+import SearchExample from '../views/SearchExample.vue';
+
+import Example from '../views/Example.vue';
+import Dashboard from '../views/admin/Dashboard.vue';
+import Sidebar from '../layouts/admin/layouts/Sidebar.vue';
 
 //import { routeGuard } from '../auth'
 
@@ -11,13 +20,17 @@ const routes = [
 		path: '/',
 		name: 'Home',
 		component: () => import('../layouts/Index.vue'),
+		meta: {
+			title: 'Home',
+		},
+
 		children: [
 			{
 				path: '',
 				component: Home,
 			},
 			{
-				path: 'movie/:id',
+				path: 'movie/:name',
 				name: 'MovieDetails',
 				component: MovieDetails,
 				props: true,
@@ -31,9 +44,62 @@ const routes = [
 		],
 	},
 
-	{ path: '/test', component: Test },
-	{ path: '/test', component: Home },
+	{
+		path: '/test',
+		component: Test,
+		meta: {
+			title: 'Home',
+		},
+	},
+	{
+		path: '/searchexample',
+		component: SearchExample,
+		meta: {
+			title: 'Home',
+		},
+	},
+	{
+		path: '/example',
+		name: 'Example',
+		component: Example,
+	},
 
+	{
+		path: '/sidebar',
+		component: Sidebar,
+	},
+	{
+		path: '/admin',
+		name: 'Admin',
+		component: () => import('../layouts/admin/index.vue'),
+		redirect: '/admin/dashboard',
+		children: [
+			{
+				path: 'dashboard',
+				name: 'Dashboard',
+				component: Dashboard,
+			},
+
+			{
+				path: 'movies',
+				name: 'Movies',
+				component: Movies,
+			},
+			{
+				path: 'reservations',
+				name: 'Reservation',
+				component: Reservatons,
+			},
+			{
+				path: 'scheduledmovies',
+				name: 'ScheduledMovies',
+				component: ScheduledMovies,
+			},
+
+			{ path: 'profile', name: 'Profile', component: Profile },
+		],
+	},
+	{ path: '/uploads', component: () => import('../views/UploadImage.vue') },
 	{ path: '/:path(.*)', component: NotFound },
 ];
 
