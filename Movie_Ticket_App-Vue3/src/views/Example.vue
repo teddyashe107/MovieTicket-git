20232a9cea0d938522a52f7dc7bcb87d099d8d40
teddyashe17 <teddyashe107@gmail.com>
2021-12-11 00:24:20 +0300

    new componnents

---
+++ Movie_Ticket_App-Vue3/src/views/Example.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {computed, ref, watch} from'vue'
+const data = ref(0)
+function add() {
+    data.value++
+}
+
+watch (data, (datadd, prevDatsa) => {
+    console.log(datadd)
+})
+
+computed()
+
+
+</script>
+
+
+<template>
+  <div class="container mx-auto flex flex-col items-center mt-32">
+    <h1>{{ data }}</h1>
+
+    <button @click="add" class="btn btn-info">count</button>
+  </div>
+</template>
