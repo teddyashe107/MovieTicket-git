8e9ac0c4540f54397a1adbbfeb6e19a53ad92d2d
teddyashe17 <teddyashe107@gmail.com>
2021-12-11 00:18:28 +0300

    some changes to the user and admin section

---
+++ Movie_Ticket_App-Vue3/src/views/admin/Reservations.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="col-span-12">
+    <BaseCard>
+      <template v-slot:cardHeader>
+        <div class="card-header">
+          <div class="card-title py-3">Recent Reservations</div>
+        </div>
+      </template>
+      <div
+        class="block w-full overflow-x-auto whitespace-nowrap borderless hover"
+      >
+        <div
+          class="dataTable-wrapper dataTable-loading no-footer fixed-columns"
+        >
+          <div
+            class="
+              dataTable-container
+              block
+              w-full
+              overflow-x-auto
+              whitespace-nowrap
+              borderless
+              hover
+            "
+          >
+            <table class="table-3 dataTable-table max-w-full w-full">
+              <thead>
+                <tr class="">
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Reservation Id
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Coustmer Name
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Movie
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Status
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Paid
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Date
+                  </th>
+                  <th
+                    class="
+                      text-left
+                      border-b
+                      pb-3
+                      mb-3
+                      text-gray-500
+                      font-semibold
+                    "
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="hover:bg-gray-100 cursor-pointer"
+                  v-for="(n, index) in 8"
+                  :key="index"
+                >
+                  <td class="text-xs py-5 px-4">
+                    {{ index + 1 }}
+                  </td>
+
+                  <td class="text-xs">Jhon {{ index + 1 }}</td>
+                  <td class="py-5">
+                    <div class="flex">
+                      <img
+                        class="w-9 h-9 rounded-full mr-2"
+                        src="/images/avatar.jpg"
+                        alt=""
+                      />
+                      <img
+                        class="w-9 h-9 rounded-full mr-2"
+                        src="/images/avatar.jpg"
+                        alt=""
+                      />
+                    </div>
+                  </td>
+                  <td class="py-5">
+                    <span
+                      class="
+                        px-3
+                        py-1
+                        rounded-full
+                        text-primary
+                        bg-green-700
+                        text-white
+                        border border-primary
+                        mr-3
+                        text-xs
+                      "
+                      >Painding</span
+                    >
+                  </td>
+                  <td class="py-5">{{ 3.34 * index + 1 }}%</td>
+                  <td class="py-5">12-02-20</td>
+                  <td class="py-5">
+                    <BaseBtn
+                      rounded
+                      class="
+                        border border-primary
+                        text-white
+                        bg-pink-900
+                        hover:bg-primary hover:text-white
+                      "
+                    >
+                      Delete
+                    </BaseBtn>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="dataTable-bottom">
+            <div class="dataTable-info">Showing 1 to 8 of 8 entries</div>
+            <nav class="dataTable-pagination">
+              <ul class="dataTable-pagination-list"></ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </BaseCard>
+  </div>
+</template>
