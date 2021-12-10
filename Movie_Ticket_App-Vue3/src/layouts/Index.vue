7ec097a8280caae7d2a0ef7526e8efa110d5f07c
teddyashe17 <teddyashe107@gmail.com>
2021-12-11 00:22:59 +0300

    additional  layouts

--- Movie_Ticket_App-Vue3/src/layouts/Index.vue
+++ Movie_Ticket_App-Vue3/src/layouts/Index.vue
@@ -8,45 +8,27 @@ import Footer from './Footer.vue'
 <template>
   <div class="app-admin-wrap-layout-2">
     <Header />
-    <div class="main-content-wrap mt-auto">
-      <main>
-        <div class="main-content-wrap flex flex-col flex-grow print-area pt-10">
-          <div>
-            <router-view />
-          </div>
-
-          <Footer />
-        </div>
-      </main>
-    </div>
   </div>
+  <div class="">
+    <main>
+      <div class="flex flex-col">
+        <div>
+          <router-view />
+        </div>
+      </div>
+    </main>
+  </div>
+
+  <Footer />
 </template>
 
 
 <style lang="scss" scoped>
 .app-admin-wrap-layout-2 {
+  // @media screen and (max-width: 991px) {
   width: 100%;
-  height: 100%;
-  .main-content-wrap {
-    width: calc(100% - 120px);
-    margin-left: 90px;
-    // min-height: 100vh;
-    margin-top: 80px;
-    transition: all 0.24s ease-in-out;
-    .main-content-body {
-      min-height: calc(100vh - 80px);
-    }
-    &.full {
-      width: 100%;
-      margin-left: 0px;
-      transition: all 0.24s ease-in-out;
-    }
-    @media screen and (max-width: 991px) {
-      width: 100%;
-      margin-left: 0px;
-      padding-right: 16px;
-      padding-left: 16px;
-    }
-  }
+  margin-left: 0px;
+
+  // }
 }
 </style>
