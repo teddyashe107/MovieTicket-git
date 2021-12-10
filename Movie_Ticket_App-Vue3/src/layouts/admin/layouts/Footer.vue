d427e7a10330413af67f46bc9ae93cad48cd9161
teddyashe17 <teddyashe107@gmail.com>
2021-12-10 22:34:27 +0300

    done with page footer

---
+++ Movie_Ticket_App-Vue3/src/layouts/admin/layouts/Footer.vue
@@ -0,0 +1,39 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <footer class="mt-10">
+    <div class="container-fluid mx-auto">
+      <BaseCard class="">
+        <div class="flex flex-col lg:flex-row items-center">
+          <div>
+            <a
+              target="_blank"
+              href="https://ui-lib.com/downloads/aatrox-vue-dashboard"
+              style="bottom: 14px; right: 80px"
+              class="
+                py-3
+                px-5
+                inline-block
+                bg-green-500
+                text-white
+                btn
+                rounded
+                font-normal
+                leading-4
+                ripple
+                relative
+                lg:fixed
+              "
+              >Get Aatrox Pro</a
+            >
+          </div>
+          <div>
+            <p>All rights reserved © UI Lib 2021</p>
+          </div>
+        </div>
+      </BaseCard>
+    </div>
+  </footer>
+</template>
